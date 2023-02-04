252fff235086c8bc072e349764916f00864d9c11
nebojsa_jacovic <nebojsa.jacovic@alterset.net>
2023-02-04 11:39:56 +0100

    Add tests

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,18 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import { UserService } from './user.service';
 
 describe('UserController', () => {
   let controller: UserController;
+  let spyService: UserService;
 
   beforeEach(async () => {
+    const ServiceProvider = {
+      provide: UserService,
+      useFactory: () => ({
+        findById: jest.fn(() => {{}}),
+      }),
+    };
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
+      providers: [UserService, ServiceProvider],
     }).compile();
 
     controller = module.get<UserController>(UserController);
+    spyService = module.get<UserService>(UserService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('calls findById method', () => {
+    const id = '1';
+    controller.getUser(id);
+    expect(spyService.findById).toHaveBeenCalled();
   });
 });
--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,12 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
 
+class ServiceMock {
+  getUser(user: any) {
+    return user;
+  }
+  async findByEmail(email: string) {
+    return email;
+  }
+  async findById(id: string) {
+    return { id };
+  }
+  async create(firstName: string, email: string, hashedPassword: string) {
+    return { firstName, email, hashedPassword };
+  }
+}
+
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: UserService,
+          useClass: ServiceMock,
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
@@ -14,5 +35,36 @@ describe('UserService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should call getUser method with expected params', async () => {
+    const getUserSpy = jest.spyOn(service, 'getUser');
+    const user: any = {
+      firstName: 'Joe',
+      email: 'joe@joe.com',
+      password: 'password',
+    };
+    service.getUser(user);
+    expect(getUserSpy).toHaveBeenCalledWith(user);
+  });
+
+  it('should call findByEmail method with expected params', async () => {
+    const findByEmailSpy = jest.spyOn(service, 'findByEmail');
+    const email = 'email';
+    service.findByEmail(email);
+    expect(findByEmailSpy).toHaveBeenCalledWith(email);
+  });
+
+  it('should call findById method with expected params', async () => {
+    const findByIdSpy = jest.spyOn(service, 'findById');
+    const id = '1';
+    service.findById(id);
+    expect(findByIdSpy).toHaveBeenCalledWith(id);
+  });
+
+  it('should call create method with expected params', async () => {
+    const createSpy = jest.spyOn(service, 'create');
+    service.create('Joe', 'joe@joe.com', 'hash');
+    expect(createSpy).toHaveBeenCalledWith('Joe', 'joe@joe.com', 'hash');
   });
 });
